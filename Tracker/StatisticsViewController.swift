6ebfa72498a50704e6aabe8f4285b9f90d2837f0
KirillKaszycki <kashitskiikirill@gmail.com>
2024-06-25 22:04:53 +0500

    Initial commit

---
+++ Tracker/StatisticsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticsViewController.swift
+//  Tracker
+//
+//  Created by Кирилл Кашицкий on 25.06.2024.
+//
+
+import Foundation
--- Tracker/TrackersView/TrackersViewController.swift
+++ Tracker/TrackersView/TrackersViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TrackersViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
